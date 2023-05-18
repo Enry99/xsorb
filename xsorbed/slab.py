5afa4176eef2b34bccaaf2fa9da5afd29b38d35a
Enry99 <enri.p25@gmail.com>
2023-05-18 15:25:24 +0200

    CRITICAL fix of minus sign that resulted in wrong fixing of slab atoms/layers

--- xsorbed/slab.py
+++ xsorbed/slab.py
@@ -56,7 +56,7 @@ class Slab:
         ###############################################################
 
         #reindex mapping before sorting by z for fixing atoms by index#
-        self.reindex_map = np.argsort(-self.slab_ase.positions[:, 2], kind='stable')
+        self.reindex_map = np.argsort(self.slab_ase.positions[:, 2], kind='stable')
         #NOTE: here we used np.argsort to get the indices, while ase.sort (which uses sorted library) to actually sort the elements.
         # Check if same result (possible problems with very similar numbers)         
         ###############################################################      
