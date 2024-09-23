15b803ab3430341c6a4a3846859e6b78dcafe383
Enry99 <enri.p25@gmail.com>
2024-09-24 00:50:09 +0200

    Created new settings file in yaml, updated init

--- xsorb/__init__.py
+++ xsorb/__init__.py
@@ -1,1 +1,15 @@
+#Created on Fri 6 Feb 2023
+
+#@author: Enrico Pedretti
+#@author: Paolo Restuccia
+
+"""
+Xsorb is a lightweight, flexible and user-friendly tool to generate and run ab initio calculations of molecules adsorbed over substrates,
+relying on VASP and Quantum Espresso for DFT calculations and on ASE & Pymatgen to generate the adsorption structures
+
+The documentation can be found at:
+https://gitlab.com/triboteam/xsorbed/-/wikis/home
+
+"""
+
 __version__ = "2.2"
