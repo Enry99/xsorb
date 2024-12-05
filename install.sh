e59ec482c5a1c48f773e3ee72874be581d8b202a
Enry99 <enri.p25@gmail.com>
2024-12-05 18:58:52 +0100

    Updated install.sh, increased max steps in mlopt

--- install.sh
+++ install.sh
@@ -2,14 +2,11 @@ xsorb_path=$(pwd)
 
 if !(grep -q ${xsorb_path} ~/.bashrc)
 then
-echo "export PATH=${xsorb_path}/xsorbed:\${PATH}" >> ~/.bashrc
+echo "export PATH=${xsorb_path}/xsorb/bin:\${PATH}" >> ~/.bashrc
 echo "export PYTHONPATH=${xsorb_path}:\${PYTHONPATH}" >> ~/.bashrc
 else
 echo "xsorb already found on PATH. Please remove it from .bashrc if you want to change the script location."
 fi
 
-chmod +x xsorbed/xsorb
-chmod +x xsorbed/xfrag
-
-
-source ~/.bashrc
+chmod +x xsorb/bin/xsorb
+chmod +x xsorb/bin/xsorb-ml-opt
